ea158209b7d582823490830303ac357bf1834af3
Martin Viljoen <mailmartinviljoen@gmail.com>
2025-01-24 19:34:21 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -42,8 +42,23 @@ echo "HTTP Connect type:" $HTTP_Connect_Type
 #echo $HA_LongLiveToken
 
 echo "Getting bearer token from solar service provider's API."
-ServerAPIBearerToken=$(curl -s -k -X POST -H "Content-Type: application/json" https://api.sunsynk.net/oauth/token -d '{"areaCode": "sunsynk","client_id": "csp-web","grant_type": "password","password": "'"$sunsynk_pass"'","source": "sunsynk","username": "'"$sunsynk_user"'"}' | jq -r '.data.access_token')
+#ServerAPIBearerToken=$(curl -s -k -X POST -H "Content-Type: application/json" https://api.sunsynk.net/oauth/token -d '{"areaCode": "sunsynk","client_id": "csp-web","grant_type": "password","password": "'"$sunsynk_pass"'","source": "sunsynk","username": "'"$sunsynk_user"'"}' | jq -r '.data.access_token')
+#echo "Bearer Token length:" ${#ServerAPIBearerToken}
 
+while true; do
+    # Fetch the token using curl
+   ServerAPIBearerToken=$(curl -s -k -X POST -H "Content-Type: application/json" https://api.sunsynk.net/oauth/token -d '{"areaCode": "sunsynk","client_id": "csp-web","grant_type": "password","password": "'"$sunsynk_pass"'","source": "sunsynk","username": "'"$sunsynk_user"'"}' | jq -r '.data.access_token')
+    # Check if the token length is at least 5 characters
+if [ ${#ServerAPIBearerToken} -ge 5 ]
+then
+	echo "Valid token retrieved."
+	break
+else
+	echo "Invalid token received: Retrying..."
+	ServerAPIBearerToken=$(curl -s -k -X POST -H "Content-Type: application/json" https://api.sunsynk.net/oauth/token -d '{"areaCode": "sunsynk","client_id": "csp-web","grant_type": "password","password": "'"$sunsynk_pass"'","source": "sunsynk","username": "'"$sunsynk_user"'"}' | jq -r '.data.access_token')
+	sleep 2
+fi
+done
 echo "Bearer Token length:" ${#ServerAPIBearerToken}
 
 #BOF Check if Token is valid
